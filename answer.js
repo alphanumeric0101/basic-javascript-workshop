f5fb9ee86cff59aa98c10e2f9bdced1d2b1cf891
alphanumeric0101 <alphanumeric@openmailbox.org>
2015-11-13 19:58:09 +0000

    3. string at index provided

--- answer.js
+++ answer.js
@@ -4,4 +4,14 @@ function firstLetter(input) {
     return input[0];
 }
 
-//
+//returns the last letter of given string
+
+function lastLetter(input) {
+    return input[input.length - 1];
+}
+
+// takes a string and a number and returns the character at the number index of the string
+
+function indexOfString(str, num) {
+    return str[num];
+}
